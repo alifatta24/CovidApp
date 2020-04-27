982c1c04ae04224ce753053fa0ec8ee0075078cc
alifatta24 <alifatta24@gmail.com>
2020-04-26 22:30:46 -0400

    commit 4 stats

---
+++ app/src/main/java/fau/amoracen/speechmap/CountryInfo.java
@@ -0,0 +1,90 @@
+package fau.amoracen.speechmap;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
+public class CountryInfo implements Parcelable {
+    String mCovidCountry, mTodayCases, mDeaths, mTodayDeaths, mRecovered, mActive, mCritical, mFlags;
+    int mCases;
+
+    public CountryInfo(String mCovidCountry, int mCases, String mTodayCases, String mDeaths, String mTodayDeaths, String mRecovered, String mActive, String mCritical, String mFlags) {
+        this.mCovidCountry = mCovidCountry;
+        this.mCases = mCases;
+        this.mTodayCases = mTodayCases;
+        this.mDeaths = mDeaths;
+        this.mTodayDeaths = mTodayDeaths;
+        this.mRecovered = mRecovered;
+        this.mActive = mActive;
+        this.mCritical = mCritical;
+        this.mFlags = mFlags;
+    }
+
+    public String getmCovidCountry() {
+        return mCovidCountry;
+    }
+
+    public int getmCases() {
+        return mCases;
+    }
+
+    public String getmTodayCases() {
+        return mTodayCases;
+    }
+
+    public String getmDeaths() {
+        return mDeaths;
+    }
+
+
+
+    public String getmActive() {
+        return mActive;
+    }
+
+
+
+    public String getmFlags() {
+        return mFlags;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mCovidCountry);
+        dest.writeInt(this.mCases);
+        dest.writeString(this.mTodayCases);
+        dest.writeString(this.mTodayDeaths);
+        dest.writeString(this.mActive);
+        dest.writeString(this.mFlags);
+    }
+
+    protected CountryInfo(Parcel in) {
+        this.mCovidCountry = in.readString();
+        this.mCases = in.readInt();
+        this.mTodayCases = in.readString();
+        this.mDeaths = in.readString();
+        this.mTodayDeaths = in.readString();
+        this.mRecovered = in.readString();
+        this.mActive = in.readString();
+        this.mCritical = in.readString();
+        this.mFlags = in.readString();
+    }
+
+    public static final Creator<CountryInfo> CREATOR = new Creator<CountryInfo>() {
+        @Override
+        public CountryInfo createFromParcel(Parcel source) {
+            return new CountryInfo(source);
+        }
+
+        @Override
+        public CountryInfo[] newArray(int size) {
+            return new CountryInfo[size];
+        }
+    };
+}
--- app/src/main/java/fau/amoracen/speechmap/StatsFragment.java
+++ app/src/main/java/fau/amoracen/speechmap/StatsFragment.java
@@ -1,65 +1,136 @@
 package fau.amoracen.speechmap;
-
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class StatsFragment extends Fragment {
-    private final int REQ_CODE = 100;
-    private TextView inputTextView;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.stats_layout, container, false);
+    RecyclerView rvCovidCountry;
+    ProgressBar progressBar;
+    CountryAdapter covidCountryAdapter;
+
+    private static final String TAG = StatsFragment.class.getSimpleName();
+    List<CountryInfo> covidCountries;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.stats_layout, container, false);
+
+        // set has option menu as true because we have menu
+        setHasOptionsMenu(true);
+
+        // call view
+        rvCovidCountry = root.findViewById(R.id.rvCovidCountry);
+        progressBar = root.findViewById(R.id.progress_circular_country);
+        rvCovidCountry.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvCovidCountry.getContext(), DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(ContextCompat.getDrawable(getContext(), R.drawable.line_divider));
+        rvCovidCountry.addItemDecoration(dividerItemDecoration);
+
+        //call list
+        covidCountries = new ArrayList<>();
+
+        // call Volley method
+        getDataFromServerSortTotalCases();
+
+        return root;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        inputTextView = view.findViewById(R.id.inputEditText);
-        ImageView speakImageView = view.findViewById(R.id.speakImageView);
+    private void showRecyclerView() {
+        covidCountryAdapter = new CountryAdapter(covidCountries, getActivity());
+        rvCovidCountry.setAdapter(covidCountryAdapter);
 
-        speakImageView.setOnClickListener(new View.OnClickListener() {
+        ItemClick.addTo(rvCovidCountry).setOnItemClickListener(new ItemClick.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Need to speak");
-                try {
-                    startActivityForResult(intent, REQ_CODE);
-                } catch (ActivityNotFoundException a) {
-                    Toast.makeText(getContext(), "Sorry your device is not supported", Toast.LENGTH_SHORT).show();
-                }
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                //showSelectedCovidCountry(covidCountries.get(position));
             }
         });
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQ_CODE) {
-            if (resultCode == Activity.RESULT_OK && null != data) {
-                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                assert result != null;
-                inputTextView.setText(result.get(0));
+
+    private void getDataFromServerSortTotalCases() {
+        String url = "https://corona.lmao.ninja/v2/countries";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressBar.setVisibility(View.GONE);
+                if (response != null) {
+                    Log.e(TAG, "onResponse: " + response);
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject data = jsonArray.getJSONObject(i);
+
+                            // Extract JSONObject inside JSONObject
+                            JSONObject countryInfo = data.getJSONObject("countryInfo");
+
+                            covidCountries.add(new CountryInfo(
+                                    data.getString("country"), data.getInt("cases"),
+                                    data.getString("todayCases"), data.getString("deaths"),
+                                    data.getString("todayDeaths"), data.getString("recovered"),
+                                    data.getString("active"), data.getString("critical"),
+                                    countryInfo.getString("flag")
+                            ));
+                        }
+
+                        // sort descending
+                        Collections.sort(covidCountries, new Comparator<CountryInfo>() {
+                            @Override
+                            public int compare(CountryInfo o1, CountryInfo o2) {
+                                if (o1.getmCases() > o2.getmCases()) {
+                                    return -1;
+                                } else {
+                                    return 1;
+                                }
+                            }
+                        });
+
+                        // Action Bar Title
+                        getActivity().setTitle(jsonArray.length() + " countries");
+
+                        showRecyclerView();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressBar.setVisibility(View.GONE);
+                        Log.e(TAG, "onResponse: " + error);
+                    }
+                });
+        Volley.newRequestQueue(getActivity()).add(stringRequest);
     }
+
+
 }
