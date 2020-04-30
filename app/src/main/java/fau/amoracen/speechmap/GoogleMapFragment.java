2adce151207b54a83a703381526b8b22294888eb
alifatta24 <alifatta24@gmail.com>
2020-04-29 23:49:04 -0400

    commit 7 Health

--- app/src/main/java/fau/amoracen/speechmap/GoogleMapFragment.java
+++ app/src/main/java/fau/amoracen/speechmap/GoogleMapFragment.java
@@ -1,27 +1,39 @@
 package fau.amoracen.speechmap;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class GoogleMapFragment extends Fragment  {
 
+    TextView textView;
+    TextView textView2;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.news_layout, container, false);
+    public GoogleMapFragment() {
+
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.news_layout, container, false);
 
+        textView = v.findViewById(R.id.textViewLink);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        textView = v.findViewById(R.id.textViewLink2);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        return v;
     }
 
 }
